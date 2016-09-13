b63408426b004b03e99a4dee1cd0113bfd2fe9d9
tmarkam <tmarkam@gmail.com>
2016-09-13 00:00:00 -0700

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,8 +1,8 @@
 # plot2.R 
 # author: Ted M
 # date: Sep 11, 2016
-# title: Coursera Exploratory Data Analysis Week 4 exercise, plot 1
-# function: display a histogram of household global minute-averaged active power consumption for the period 2/1/2007-2/2/2007
+# title: Coursera Exploratory Data Analysis Week 4 exercise, plot 2
+# function: display a plot of Global Active Power for the period 2/1/2007-2/2/2007
 # notes: this script uses the "Electric power sonumption" dataset from UC Irvine Machine Learning Repository
 # we use the "readr" library - as it's less memory-intensive than the base read functions
 library(readr)
@@ -26,7 +26,7 @@ hpc_sub<-subset(hpc,hpc$Date >= as.Date("2007-02-01") & hpc$Date <= as.Date("200
 # combine the date and time columns into a timestamp
 hpc_sub$Timestamp <- hpc_sub$Date + seconds(hpc_sub$Time)
 
-# plot the histogram, with colors and labels as specified in the exercise
+# plot the data, with colors and labels as specified in the exercise
 # note: to test, comment out the png and dev.off statements
 png(filename = "plot2.png", width = 480, height = 480)
 plot(hpc_sub$Timestamp, hpc_sub$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
