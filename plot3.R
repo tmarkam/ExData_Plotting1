d7412a5bdb6bb00fba0770d54fa4ec476f274aae
tmarkam <tmarkam@gmail.com>
2016-09-13 00:01:39 -0700

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,7 +1,7 @@
 # plot3.R 
 # author: Ted M
 # date: Sep 11, 2016
-# title: Coursera Exploratory Data Analysis Week 4 exercise, plot 1
+# title: Coursera Exploratory Data Analysis Week 4 exercise, plot 3
 # function: display a plot of energy sub-metering for the period 2/1/2007-2/2/2007
 # notes: this script uses the "Electric power consumption" dataset from UC Irvine Machine Learning Repository
 # we use the "readr" library - as it's less memory-intensive than the base read functions
@@ -26,7 +26,7 @@ hpc_sub<-subset(hpc,hpc$Date >= as.Date("2007-02-01") & hpc$Date <= as.Date("200
 # combine the date and time columns into a timestamp
 hpc_sub$Timestamp <- hpc_sub$Date + seconds(hpc_sub$Time)
 
-# plot the histogram, with colors and labels as specified in the exercise
+# plot the data, with colors and labels as specified in the exercise
 # note: to test, comment out the png and dev.off statements
 png(filename = "plot3.png", width = 480, height = 480)
 plot(hpc_sub$Timestamp, hpc_sub$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
